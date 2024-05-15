78d4ce515f4ea80b7b80b347177deb490b55b4ac
SantiagoVanegas <luis.vanegas01@uceva.edu.co>
2024-05-15 11:23:47 -0500

    commit otro mas

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,11 @@ import 'package:memes/config/router/app_router.dart';
 import 'package:memes/config/theme/app_theme.dart';
 // Importa el paquete shared_preferences
 
-<<<<<<< HEAD
-=======
-//Segundo commit Vangeas
-//PrimerCommit Vanegas
-
->>>>>>> 3dbc770a3a2271818ce190aae23bbdcaa100afdd
 void main() {
   runApp(const MyApp());
 }
 
-<<<<<<< HEAD
 //hol
-=======
->>>>>>> 3dbc770a3a2271818ce190aae23bbdcaa100afdd
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
