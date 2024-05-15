3dbc770a3a2271818ce190aae23bbdcaa100afdd
SantiagoVanegas <luis.vanegas01@uceva.edu.co>
2024-05-15 10:56:53 -0500

    Segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:memes/config/router/app_router.dart';
 import 'package:memes/config/theme/app_theme.dart';
 // Importa el paquete shared_preferences
 
+//Segundo commit Vangeas
 //PrimerCommit Vanegas
 
 void main() {
